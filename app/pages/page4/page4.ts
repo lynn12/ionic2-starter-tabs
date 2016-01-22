34653c31227760ecd6a61e807cb4d22cee1d9b16
lynn <kongxlmusic@163.com>
2016-01-22 16:28:26 +0800

    add user list

---
+++ app/pages/page4/page4.ts
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-framework/ionic';
+
+
+@Page({
+  templateUrl: 'build/pages/page4/page4.html'
+})
+export class Page4 {
+  constructor() {
+
+  }
+}
---
+++ app/vo/User.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/1/22.
+ */
+
+interface  User {
+    id:string;
+    name:string;
+    des:string;
+    age:number;
+}
