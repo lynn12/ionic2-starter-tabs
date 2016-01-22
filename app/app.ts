5679a95d127c1b7ccb0238e47caf5ff1d3933ab5
lynn <kongxlmusic@163.com>
2016-01-22 08:47:02 +0800

    init

--- app/app.ts
+++ app/app.ts
@@ -1,33 +1,18 @@
-import {App, Platform} from 'ionic-framework/ionic';
+import {App, Platform, Config} from 'ionic-framework/ionic';
 import {TabsPage} from './pages/tabs/tabs';
 
-// https://angular.io/docs/ts/latest/api/core/Type-interface.html
-import {Type} from 'angular2/core';
-
-
 @App({
-  template: '<ion-nav [root]="rootPage"></ion-nav>',
-  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+  template: '<ion-nav id="nav" [root]="root" #content></ion-nav>'
+  // Check out the config API docs for more info
+  // http://ionicframework.com/docs/v2/api/config/Config/
+  config: {}
 })
 export class MyApp {
-  rootPage: Type = TabsPage;
-
   constructor(platform: Platform) {
+    this.root = TabsPage;
+
     platform.ready().then(() => {
-      // The platform is now ready. Note: if this callback fails to fire, follow
-      // the Troubleshooting guide for a number of possible solutions:
-      //
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      //
-      // First, let's hide the keyboard accessory bar (only works natively) since
-      // that's a better default:
-      //
-      // Keyboard.setAccessoryBarVisible(false);
-      //
-      // For example, we might change the StatusBar color. This one below is
-      // good for dark backgrounds and light text:
-      // StatusBar.setStyle(StatusBar.LIGHT_CONTENT)
+      // Do any necessary cordova or native calls here now that the platform is ready
     });
   }
 }
