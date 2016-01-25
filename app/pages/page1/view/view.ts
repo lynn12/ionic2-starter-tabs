51e9dbe3e7d7c7a973d6b45b6d62f2cde3d701a7
lynn <kongxlmusic@163.com>
2016-01-25 17:33:20 +0800

    add user detail

---
+++ app/pages/page1/view/view.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2016/1/25.
+ */
+import {Page} from 'ionic-framework/ionic';
+import {NavController, NavParams} from 'ionic-framework/ionic';
+import {User} from '../../../vo/User';
+
+@Page({
+    templateUrl: 'build/pages/page1/view/view.html',
+})
+export  class  userView() {
+    public  var user:User;
+    constructor(nav: NavController, params:NavParams) {
+        this.user = params.data;
+    }
+
+}
